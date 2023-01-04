81ae19c706ca735fdb1998455497c3af8e5d7d91
jaeho <ovsoce@gmail.com>
2023-01-05 01:01:21 +0900

    feat: 플레이그라운드 환경 조성

--- playground/src/main/java/io/github/ovso/playground/Playground.kt
+++ playground/src/main/java/io/github/ovso/playground/Playground.kt
@@ -1,8 +1,20 @@
 package io.github.ovso.playground
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+
 object Playground {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Hello World")
+        println("Hello World!")
+        runBlocking {
+            aa()
+        }
+    }
+
+    private suspend fun aa() {
+        coroutineScope {
+            println("zzzzz")
+        }
     }
 }
