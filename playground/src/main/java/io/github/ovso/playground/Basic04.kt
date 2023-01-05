cca1e8ae163b88d570f46f435a05e857ac43f220
jaeho <ovsoce@gmail.com>
2023-01-06 06:25:28 +0900

    docs: 주석 추가

--- playground/src/main/java/io/github/ovso/playground/Basic04.kt
+++ playground/src/main/java/io/github/ovso/playground/Basic04.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.*
 object Basic04 {
     @JvmStatic
     fun main(args: Array<String>) = runBlocking {
-        doWorld()
+        doWorld() // 코루틴 안의 코루틴이 완료될 때까지 대기합니다.
         println("Done")
     }
 
