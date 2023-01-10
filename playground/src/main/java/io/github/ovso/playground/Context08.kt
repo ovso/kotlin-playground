9e88c330edb80acc62df6c85caead80cc6c97a53
jaeho <ovsoce@gmail.com>
2023-01-10 22:52:53 +0900

    feat: 부모 코루틴의 책임에 대하여

---
+++ playground/src/main/java/io/github/ovso/playground/Context08.kt
@@ -0,0 +1,27 @@
+@file:Suppress("SpellCheckingInspection")
+
+package io.github.ovso.playground
+
+import kotlinx.coroutines.*
+
+object Context08 {
+    /*
+    코루틴 네임을 지정합니다.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking(CoroutineName("main")) {
+        log("Started main coroutine")
+        // run two background value computations
+        val v1 = async(CoroutineName("v1coroutine")) {
+            delay(500)
+            log("Computing v1")
+            252
+        }
+        val v2 = async(CoroutineName("v2coroutine")) {
+            delay(1000)
+            log("Computing v2")
+            6
+        }
+        log("The answer for v1 / v2 = ${v1.await() / v2.await()}")
+    }
+}
