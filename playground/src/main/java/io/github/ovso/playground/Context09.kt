573f306454d50e8ea7ad16c93738a52b59baf71b
jaeho <ovsoce@gmail.com>
2023-01-10 23:07:51 +0900

    feat: 부모 코루틴의 책임에 대하여

---
+++ playground/src/main/java/io/github/ovso/playground/Context09.kt
@@ -0,0 +1,18 @@
+@file:Suppress("SpellCheckingInspection")
+
+package io.github.ovso.playground
+
+import kotlinx.coroutines.*
+
+object Context09 {
+    /*
+    코루틴 컨텍스트를 + 연산자로 결합하여 사용할 수 있습니다.
+    이를 `컨텍스트 요소 결합` 이라고 합니다.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking<Unit> {
+        launch(Dispatchers.Default + CoroutineName("test")) {
+            println("I'm working in thread ${Thread.currentThread().name}")
+        }
+    }
+}
