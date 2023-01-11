f21bae78befd206fefccb6cd5c3f06d368487408
jaeho <ovsoce@gmail.com>
2023-01-12 06:22:53 +0900

    feat: Context11

--- playground/src/main/java/io/github/ovso/playground/Context11.kt
+++ playground/src/main/java/io/github/ovso/playground/Context11.kt
@@ -2,10 +2,20 @@
 
 package io.github.ovso.playground
 
+import io.github.ovso.playground.Context11.threadLocal
 import kotlinx.coroutines.*
 
 object Context11 {
-    @JvmStatic
-    fun main(args: Array<String>) = runBlocking {
-    }
+    val threadLocal = ThreadLocal<String?>() // declare thread-local variable
 }
+    fun main(args: Array<String>) = runBlocking {
+        threadLocal.set("main")
+        println("Pre-main, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+        val job = launch(Dispatchers.Default + threadLocal.asContextElement(value = "launch")) {
+            println("Launch start, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+            yield()
+            println("After yield, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+        }
+        job.join()
+        println("Post-main, current thread: ${Thread.currentThread()}, thread local value: '${threadLocal.get()}'")
+    }
