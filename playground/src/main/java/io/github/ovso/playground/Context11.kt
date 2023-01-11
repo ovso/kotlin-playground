4bc06365b858c6ff35ad07eafb932c3eee85c1e7
jaeho <ovsoce@gmail.com>
2023-01-11 16:48:52 +0900

    feat: 클래스 생성

---
+++ playground/src/main/java/io/github/ovso/playground/Context11.kt
@@ -0,0 +1,11 @@
+@file:Suppress("SpellCheckingInspection")
+
+package io.github.ovso.playground
+
+import kotlinx.coroutines.*
+
+object Context11 {
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+    }
+}
