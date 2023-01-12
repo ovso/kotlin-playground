8f7561820ce164a0ce70df38343b48d9d0538214
jaeho <ovsoce@gmail.com>
2023-01-13 06:42:34 +0900

    feat: flow03

--- playground/src/main/java/io/github/ovso/playground/flow03.kt
+++ playground/src/main/java/io/github/ovso/playground/flow03.kt
@@ -1,21 +1,19 @@
 @file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch")
 
 // 패키지를 다르게 하여 다른 클래스와의 컴파일 에러를 막습니다.
-package io.github.ovso.playground.flow02
+package io.github.ovso.playground.flow03
 
 import kotlinx.coroutines.*
 
-fun simple(): Sequence<Int> = sequence {
-    for (i in 1..3) {
-        Thread.sleep(100)
-        yield(i)
-    }
+suspend fun simple(): List<Int> {
+    delay(1000)
+    return listOf(1, 2, 3)
 }
 
-object Flow02 {
+object Flow03 {
 
     /*
-    시퀀스 빌더를 사용하여 값을 출력합니다. 그러나 기본 스레드를 차단하고 있습니다.
+    suspend 함수를 사용하여 기본스레드를 차단하지 않고 결과 목록을 반환합니다.
      */
     @JvmStatic
     fun main(args: Array<String>) = runBlocking {
