a0f6d04b8b020067b5ce658e52ccebdfbabd8094
jaeho <ovsoce@gmail.com>
2023-01-14 05:56:06 +0900

    feat: 주석 수정
    -

--- playground/src/main/java/io/github/ovso/playground/flow04.kt
+++ playground/src/main/java/io/github/ovso/playground/flow04.kt
@@ -18,7 +18,7 @@ object Flow04 {
 
     /*
     flow 빌더를 통해 메인스레들 차단하지 않고 값을 방출(emit) 합니다.
-    코루틴 범위 내에서 flow 는 suspend 키워드를 사용하지 않아도 일시정지 기능을 사용할 수 있습니다.
+    flow 는 코루틴 범위 내에서 suspend 키워드를 사용하지 않고 일시정지 기능을 사용할 수 있습니다.
     flow 빌더 내부를 보면, collectSafely 함수에서 suspend 함수를 확인할 수 있습니다.
      */
     @JvmStatic
