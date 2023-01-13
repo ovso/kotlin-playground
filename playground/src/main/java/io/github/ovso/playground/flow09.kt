dbd2364508f6d3d10ce5be3312e8f175d2a5792f
jaeho <ovsoce@gmail.com>
2023-01-14 06:32:13 +0900

    feat:

---
+++ playground/src/main/java/io/github/ovso/playground/flow09.kt
@@ -0,0 +1,17 @@
+@file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch")
+
+// 패키지를 다르게 하여 다른 클래스와의 컴파일 에러를 막습니다.
+package io.github.ovso.playground.flow09
+
+import kotlinx.coroutines.runBlocking
+
+
+// https://kotlinlang.org/docs/flow.html#transform-operator
+object Flow09 {
+
+    /*
+     */
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+    }
+}
