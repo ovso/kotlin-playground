6de33798f10424f706957f1dabeeec337ee84055
jaeho <ovsoce@gmail.com>
2023-01-16 07:06:22 +0900

    feat: flowOn 사용

--- playground/src/main/java/io/github/ovso/playground/flow15.kt
+++ playground/src/main/java/io/github/ovso/playground/flow15.kt
@@ -1,26 +1,24 @@
 @file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch")
 
 // 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
-package io.github.ovso.playground.flow14
+package io.github.ovso.playground.flow15
 
+import io.github.ovso.playground.flow14.log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.runBlocking
 
-fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
-/*
-예외가 발생합니다. 예외를 발생시키지 않으려면 flowOn 을 사용하여 쓰레드를 바꿔야 합니다.
- */
 fun simple(): Flow<Int> = flow {
     // The WRONG way to change context for CPU-consuming code in flow builder
-    kotlinx.coroutines.withContext(Dispatchers.Default) {
-        for (i in 1..3) {
-            Thread.sleep(100) // pretend we are computing it in CPU-consuming way
-            emit(i) // emit next value
-        }
+    for (i in 1..3) {
+        Thread.sleep(100) // pretend we are computing it in CPU-consuming way
+        log("Emitting $i")
+        emit(i) // emit next value
     }
-}
+}.flowOn(Dispatchers.Default)
+
 fun main() = runBlocking {
     simple().collect { value -> log("Collected $value") }
 }
