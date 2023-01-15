286026b5465e045b929f51130072ad3e4a4a4901
jaeho <ovsoce@gmail.com>
2023-01-16 06:19:12 +0900

    feat: transform 연사자 시작

--- playground/src/main/java/io/github/ovso/playground/flow08.kt
+++ playground/src/main/java/io/github/ovso/playground/flow08.kt
@@ -17,6 +17,7 @@ suspend fun performRequest(request: Int): String {
 object Flow08 {
 
     /*
+    중간 흐름 연산자 입니다.
     flow 에서의 map 연산자는 내부에서 suspend 함수를 호출하고 있습니다. 이는 정기기능(suspend) 을 사용할 수 있다는 것입니다.
     플로우 연산 중간에 비동기 요청을 map 연산자를 통해 결과에 반영할 수 있습니다.
      */
--- playground/src/main/java/io/github/ovso/playground/flow09.kt
+++ playground/src/main/java/io/github/ovso/playground/flow09.kt
@@ -3,15 +3,30 @@
 // 패키지를 다르게 하여 다른 클래스와의 컴파일 에러를 막습니다.
 package io.github.ovso.playground.flow09
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.runBlocking
 
 
-// https://kotlinlang.org/docs/flow.html#transform-operator
+suspend fun performRequest(request: Int): String {
+    delay(1000) // 장기 실행하는 비동기 작업
+    return "response $request"
+}
+
 object Flow09 {
 
     /*
+    transform 은 변환 연산자 입니다. 포괄적인 의미로 중간흐름연산자 라고도 합니다.
+    map 이나 filter 를 모방하지만 더 복잡한 연산을 수행할 수 있습니다.
      */
     @JvmStatic
     fun main(args: Array<String>) = runBlocking {
+        (1..3).asFlow() // a flow of requests
+            .transform { request ->
+                emit("Making request $request")
+                emit(performRequest(request))
+            }
+            .collect { response -> println(response) }
     }
 }
