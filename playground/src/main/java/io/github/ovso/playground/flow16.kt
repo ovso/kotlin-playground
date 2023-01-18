b8c0465e97a41963f35b26f8df4b3f38c775b857
jaeho <ovsoce@gmail.com>
2023-01-19 05:42:14 +0900

    feat: flow16

--- playground/src/main/java/io/github/ovso/playground/flow16.kt
+++ playground/src/main/java/io/github/ovso/playground/flow16.kt
@@ -3,21 +3,27 @@
 // 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
 package io.github.ovso.playground.flow16
 
-import io.github.ovso.playground.flow14.log
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 fun simple(): Flow<Int> = flow {
     for (i in 1..3) {
-        log("Emitting $i")
+        delay(100) // 비동기를 기다립니다.
         emit(i) // emit next value
     }
-}.flowOn(Dispatchers.Default) // Flow Builder에서 CPU를 많이 사용하는 코드의 컨텍스트를 변경하는 올바른 방법 입니다.
+}
 
-fun main() = runBlocking<Unit> {
+fun main() = runBlocking {
+    val time = measureTimeMillis {
+        simple().collect { value ->
+            delay(300) // 300ms 동안 처리한다고 가정합니다.
+            println(value)
+        }
+    }
+    println("Collected in $time ms") // 대략 1200 ms(숫자 3개, 각 400ms)
 }
 
 // https://kotlinlang.org/docs/flow.html#buffering
