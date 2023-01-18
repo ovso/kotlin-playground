8341dac61da46707572bc9954f2f5a1d13ecd452
jaeho <ovsoce@gmail.com>
2023-01-19 06:54:55 +0900

    feat: flow

--- playground/src/main/java/io/github/ovso/playground/flow20.kt
+++ playground/src/main/java/io/github/ovso/playground/flow20.kt
@@ -3,18 +3,24 @@
 // 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
 package io.github.ovso.playground.flow20
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.runBlocking
 
-/** 두 flow 를 결합하여 순차적으로 나타냅니다. */
+/** 두 flow 를 결합하여 순차적으로 나타냅니다.
+ * 지연 시간이 더 많은 쪽을 기준으로 처리가 됩니다.(?)
+ * */
 fun main() = runBlocking {
-    val nums = (1..3).asFlow() // numbers 1..3
-//    val nums = flowOf(1, 2, 3) // numbers 1..3
-    val strs = flowOf("one", "two", "three") // strings
-    nums.zip(strs) { a, b -> "$a -> $b" } // compose a single string
-        .collect { println(it) } // collect and print
+    val nums = (1..3).asFlow().onEach { delay(300) } // numbers 1..3 every 300 ms
+    val strs = flowOf("one", "two", "three").onEach { delay(400) } // strings every 400 ms
+    val startTime = System.currentTimeMillis() // remember the start time
+    nums.zip(strs) { a, b -> "$a -> $b" } // compose a single string with "zip"
+        .collect { value -> // collect and print
+            println("$value at ${System.currentTimeMillis() - startTime} ms from start")
+        }
 }
 
-// https://kotlinlang.org/docs/flow.html#zip
+// https://kotlinlang.org/docs/flow.html#combine
