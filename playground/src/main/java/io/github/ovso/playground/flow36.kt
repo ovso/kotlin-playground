b4cc267603ca7eb055ae9157b4a4b38ca3ab744b
jaeho.5 <ovsoce@gmail.com>
2023-02-07 22:59:12 +0900

    feat: flow 36

---
+++ playground/src/main/java/io/github/ovso/playground/flow36.kt
@@ -0,0 +1,20 @@
+@file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch", "OPT_IN_USAGE")
+
+// 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
+package io.github.ovso.playground.flow36
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+// Imitate a flow of events
+fun events(): Flow<Int> = (1..3).asFlow().onEach { delay(100) }
+
+/*
+collect 대신 launchIn 을 사용했습니다. 별도의 코루틴에서 컬렉션을 시작한다는 의미입니다.
+ */
+fun main() = runBlocking {
+    events()
+        .onEach { event -> println("Event: $event") }
+        .launchIn(this) // <--- Launching the flow in a separate coroutine
+    println("Done")
+}
