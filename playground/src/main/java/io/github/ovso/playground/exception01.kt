e4498f586441e918b73c535333d260cb686e9dff
jaeho.5 <ovsoce@gmail.com>
2023-02-14 02:37:39 +0900

    feat: channel01

---
+++ playground/src/main/java/io/github/ovso/playground/exception01.kt
@@ -0,0 +1,26 @@
+@file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch", "OPT_IN_USAGE")
+
+// 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
+package io.github.ovso.playground.exception01
+
+import kotlinx.coroutines.*
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main() = runBlocking {
+    val job = GlobalScope.launch { // root coroutine with launch
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException() // Will be printed to the console by Thread.defaultUncaughtExceptionHandler
+    }
+    job.join()
+    println("Joined failed job")
+    val deferred = GlobalScope.async { // root coroutine with async
+        println("Throwing exception from async")
+        throw ArithmeticException() // Nothing is printed, relying on user to call await
+    }
+    try {
+        deferred.await()
+        println("Unreached")
+    } catch (e: ArithmeticException) {
+        println("Caught ArithmeticException")
+    }
+}
