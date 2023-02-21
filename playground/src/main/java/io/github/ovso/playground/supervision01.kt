8052f4710f359a8183bd9eaf5b3a0a9634e14165
jaeho.5 <ovsoce@gmail.com>
2023-02-22 06:57:13 +0900

    feat: supervision01
    https://kotlinlang.org/docs/exception-handling.html#supervision-job

---
+++ playground/src/main/java/io/github/ovso/playground/supervision01.kt
@@ -0,0 +1,50 @@
+@file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch", "OPT_IN_USAGE")
+
+// 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
+package io.github.ovso.playground.supervision01
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val supervisor = SupervisorJob()
+    with(CoroutineScope(coroutineContext + supervisor)) {
+        // launch the first child -- its exception is ignored for this example (don't do this in practice!)
+        val firstChild = launch(CoroutineExceptionHandler { _, _ ->  }) {
+            println("The first child is failing")
+            throw AssertionError("The first child is cancelled")
+        }
+        // launch the second child
+        val secondChild = launch {
+            firstChild.join()
+            // Cancellation of the first child is not propagated to the second child
+            println("The first child is cancelled: ${firstChild.isCancelled}, but the second one is still active")
+            try {
+                delay(Long.MAX_VALUE)
+            } finally {
+                // But cancellation of the supervisor is propagated
+                println("The second child is cancelled because the supervisor was cancelled")
+            }
+        }
+        // wait until the first child fails & completes
+        firstChild.join()
+        println("Cancelling the supervisor")
+        supervisor.cancel()
+        secondChild.join()
+    }
+}
+
+/*
+예상
+The first child is failing
+The first child is cancelled: true, but the second one is still active
+The second child is cancelled because the supervisor was cancelled
+Cancelling the supervisor
+ */
+
+/*
+실제
+The first child is failing
+The first child is cancelled: true, but the second one is still active
+Cancelling the supervisor
+The second child is cancelled because the supervisor was cancelled
+ */
