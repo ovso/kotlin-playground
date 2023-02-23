86076d15eb74fcb7d3be8b470f08fcf06a105ae5
jaeho.5 <ovsoce@gmail.com>
2023-02-23 21:14:12 +0900

    feat: supervision02
    https://kotlinlang.org/docs/exception-handling.html#supervision-scope

---
+++ playground/src/main/java/io/github/ovso/playground/supervision02.kt
@@ -0,0 +1,32 @@
+@file:Suppress("SpellCheckingInspection", "PackageDirectoryMismatch", "OPT_IN_USAGE")
+
+// 패키지를 다르게 하여 다른 클래스와의 충돌(컴파일 에러)을 막습니다.
+package io.github.ovso.playground.supervision02
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    try {
+        supervisorScope {
+            val child = launch {
+                try {
+                    println("The child is sleeping")
+                    delay(Long.MAX_VALUE)
+                } finally {
+                    println("The child is cancelled")
+                }
+            }
+            // Give our child a chance to execute and print using yield
+            yield()
+            println("Throwing an exception from the scope")
+            throw AssertionError()
+        }
+    } catch (e: AssertionError) {
+        println("Caught an assertion error")
+    }
+}
+
+/*
+예상 출력
+
+ */
